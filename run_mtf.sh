0d20a4dccdba7a38ca9dc16ace64b07689dd094b
otoha1119 <otoha12.dev@gmail.com>
2025-10-12 18:29:22 +0000

    update:  パラメータ更新

--- run_mtf.sh
+++ run_mtf.sh
@@ -15,7 +15,7 @@ HR_DIR="/workspace/DataSet/photonCT/PhotonCT1024v2/DICOMSAVE-20240514142921-000"
 OUT_DIR="/workspace/results"
 
 # 処理パラメータ
-NUM_ROIS=150            # 1シリーズあたり抽出するROI数
+NUM_ROIS=4000            # 1シリーズあたり抽出するROI数
 DRAW_NYQUIST=0          # 1=Nyquist縦線を描画, 0=非表示
 USE_CUDA=1              # 1=CUDA使用(可能なら), 0=CPU強制
 GPU_IDS="0"             # 例: "0", "0,1", "-1"(CPU扱い)
