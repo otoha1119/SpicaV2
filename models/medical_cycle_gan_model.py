7c0dc69b2e7f0313f9a01debcaa8837c7bab2ea7
otoha1119 <otoha12.dev@gmail.com>
2025-11-18 14:26:15 +0900

    [modify] 学習率の変更

--- models/medical_cycle_gan_model.py
+++ models/medical_cycle_gan_model.py
@@ -175,9 +175,9 @@ class MedicalCycleGANModel(BaseModel):
             self.criterionIdt = torch.nn.L1Loss()
             # initialise optimisers; schedulers will be automatically created by function <BaseModel.setup>
             self.optimizer_G = torch.optim.Adam(itertools.chain(self.netG_A.parameters(), self.netG_B.parameters()),
-                                                lr=opt.lr, betas=(opt.beta1, 0.999))
+                                                lr=opt.lr_G, betas=(opt.beta1, 0.999))
             self.optimizer_D = torch.optim.Adam(itertools.chain(self.netD_A.parameters(), self.netD_B.parameters()),
-                                                lr=opt.lr, betas=(opt.beta1, 0.999))
+                                                lr=opt.lr_D, betas=(opt.beta1, 0.999))
             self.optimizers.append(self.optimizer_G)
             self.optimizers.append(self.optimizer_D)
 
