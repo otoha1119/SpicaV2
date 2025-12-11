f216dfd57281a25577a320cbfaf11d72b99c1e7e
otoha1119 <otoha12.dev@gmail.com>
2025-12-11 09:24:44 +0000

    [modify]  推論ファイルの簡略化と更新，不要な処理部分を削除しました(動作確認済)

--- inference_single.sh
+++ inference_single.sh
@@ -14,8 +14,8 @@ python inference_single.py \
   --ngf 64 \
   --input_nc 1 --output_nc 1 \
   --name SR_CycleGAN \
-  --checkpoints_dir /workspace/checkpoints \
-  --epoch 136 \
+  --checkpoints_dir /workspace/checkpoints_mac \
+  --epoch 200 \
   --use_G A \
   --input_dicom /workspace/IM_091.dcm \
   --output_dicom /workspace/results/SR_2x.dcm \
